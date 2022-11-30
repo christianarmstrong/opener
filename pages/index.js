1038c81853ccb7398d5db3210c9d9638ef815af0
Christian Armstrong <contact@thechristianarmstrong.design>
2022-11-30 07:44:38 -0600

    Opener V1.0

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
 
-const Home = () => {
-  const [userInput, setUserInput] = useState('');
 
+const Home = () => {
+  const [age_userInput, age_setUserInput] = useState('');
+  const [name_userInput, name_setUserInput] = useState('');
+  const [userInput, setUserInput] = useState('');
   const [apiOutput, setApiOutput] = useState('')
   const [isGenerating, setIsGenerating] = useState(false)
   const callGenerateEndpoint = async () => {
@@ -25,17 +27,44 @@ const Home = () => {
 
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
+    document.getElementById("enterNameText").style.display = "none";
+    document.getElementById("enterNameBox").style.display = "none";
+    document.getElementById("enterAgeText").style.display = "none";
+    document.getElementById("enterAgeBox").style.display = "none";
+    document.getElementById("enterDescriptionText").style.display = "none";
+    document.getElementById("enterDescriptionBox").style.display = "none";
+    document.getElementById("enterGenerateButton").style.display = "none";
+    document.getElementById("enterHeader").style.display = "none";
+    document.getElementById("Try Again").style.visibility = "visible"
+    document.getElementById("retryButton").style.visibility = "visible"
+    document.getElementById("retryText").style.visibility = "visible"
   }
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
+
+  const onUserChangedNameText = (event) => {
+    name_setUserInput(event.target.value);
+  };
+
+  const onUserChangedAgeText = (event) => {
+    age_setUserInput(event.target.value);
+  };
+
+  const onRetryClicked = () => {
+    window.location.href = window.location.href
+  }
+
+
+
+
   return (
     <div className="root">
       <Head>
         <title>Opener</title>
       </Head>
       <div className="container">
-        <div className="header">
+        <div className="header" id="enterHeader">
           <div className="header-title">
             <h1>Opener</h1>
           </div>
@@ -46,31 +75,33 @@ const Home = () => {
             <h2>Describe who you want to talk to and <br class="responsive" /> get the perfect opening line.</h2>
           </div>
         </div>
-        <div className="prompt-container">
+        <div className="prompt-container" id="prompts">
 
-          <div style={{ textAlign: 'left' }} className="prompt-headers">
+          <div style={{ textAlign: 'left' }} className="prompt-headers" id="enterNameText">
             <h2>What's their name?</h2>
           </div>
 
           <textarea
             className="prompt-box"
             placeholder="Enter their name"
-            value={userInput}
-            onChange={onUserChangedText}
-          />;
+            value={name_userInput}
+            onChange={onUserChangedNameText}
+            id="enterNameBox"
+          />
 
-          <div className="prompt-headers">
+          <div className="prompt-headers" id="enterAgeText">
             <h2>How old are they?</h2>
           </div>
 
           <textarea
             className="prompt-box"
             placeholder="Enter their age"
-            value={userInput}
-            onChange={onUserChangedText}
-          />;
+            value={age_userInput}
+            onChange={onUserChangedAgeText}
+            id="enterAgeBox"
+          />
 
-          <div className="prompt-headers">
+          <div className="prompt-headers" id="enterDescriptionText">
             <h2>What does their bio say <br class="responsive" /> about them?</h2>
           </div>
 
@@ -79,10 +110,11 @@ const Home = () => {
             placeholder="Describe them"
             value={userInput}
             onChange={onUserChangedText}
-          />;
+            id="enterDescriptionBox"
+          />
 
 
-          <div className="prompt-buttons">
+          <div className="prompt-buttons" id="enterGenerateButton">
             <a
               className={isGenerating ? 'generate-button loading' : 'generate-button'}
               onClick={callGenerateEndpoint}
@@ -96,7 +128,7 @@ const Home = () => {
             <div className="output">
               <div className="output-header-container">
                 <div className="output-header">
-                  <h3>Output</h3>
+                  <h3>Your Perfect Opener</h3>
                 </div>
               </div>
               <div className="output-content">
@@ -104,6 +136,15 @@ const Home = () => {
               </div>
             </div>
           )}
+          <div className="prompt-buttons " id="Try Again" >
+            <a className='retry-button' id="retryButton" onClick={onRetryClicked}>
+
+              <div className="retry" id="retryText">
+                <p>Retry</p>
+              </div>
+
+            </a>
+          </div>
         </div>
 
       </div>
