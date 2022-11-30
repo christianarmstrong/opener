d4315f5f2b2fd5af62cc6bd67e0e823f6eb98e4a
Christian Armstrong <contact@thechristianarmstrong.design>
2022-11-30 04:12:27 -0600

    V0.1 Opener App

---
+++ pages/api/generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "write me a very short and intriguing conversation opener with a girl you met on tinder that compliments her and makes her think you're smart and funny but do not mention her profile or tinder";
+const generateAction = async (req, res) => {
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+        temperature: 0.7,
+        max_tokens: 200,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,114 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Opener</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Opener</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>The best way to open a conversation.</h2>
+          </div>
+          <div className="header-subtitle">
+            <h2>Describe who you want to talk to and get the perfect opening line.</h2>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <div className="prompt-container">
+
+          <div style={{ textAlign: 'left' }} className="prompt-headers">
+            <h2>What's their name?</h2>
           </div>
-        </a>
+
+          <textarea
+            className="prompt-box"
+            placeholder="Enter their name"
+            value={userInput}
+            onChange={onUserChangedText}
+          />;
+
+          <div className="prompt-headers">
+            <h2>How old are they?</h2>
+          </div>
+
+          <textarea
+            className="prompt-box"
+            placeholder="Enter their age"
+            value={userInput}
+            onChange={onUserChangedText}
+          />;
+
+          <div className="prompt-headers">
+            <h2>What does their bio say about them?</h2>
+          </div>
+
+          <textarea
+            className="prompt-box"
+            placeholder="Describe them"
+            value={userInput}
+            onChange={onUserChangedText}
+          />;
+
+
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+
       </div>
+
     </div>
   );
 };
