2a598e5f123fe8e3232a09be0ea35b237db14889
Christian Armstrong <contact@thechristianarmstrong.design>
2022-11-30 06:04:41 -0600

    Changed Link images and Logo

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Opener" key="title" />
+        <meta property="og:description" content="The Best Way to Open a Conversation" key="description" />
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="/opener/assets/openerLinkPicture.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
