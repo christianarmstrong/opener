ac75326152eea996f997c8b06593696162851814
Christian Armstrong <contact@thechristianarmstrong.design>
2022-12-01 19:08:10 -0600

    added Hotjar tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import './styles.css';
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(3272704, 6)
+  }, [])
   return <Component {...pageProps} />
 }
 export default App;
