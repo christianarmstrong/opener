ba0b1a61a4100fb52c79f01cc7dce73c18487109
Christian Armstrong <contact@thechristianarmstrong.design>
2023-07-24 17:11:48 -0500

    Same thing

--- pages/api/webhooks/user_sync.js
+++ pages/api/webhooks/user_sync.js
@@ -1,6 +1,7 @@
 
 import { useState, useEffect } from 'react';
 import { createClient } from "@supabase/supabase-js";
+import bodyParser from 'body-parser';
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
@@ -8,31 +9,28 @@ const supabase = createClient(
 
 );
 
-
-
-
-
+const jsonParser = bodyParser.json();
 
 export default async function handler(req, res) {
-
-
-    console.log(req.body)
-    const { data, type } = req.body
-    console.log("Updating database with", data.email_addresses[0].email_address)
-    console.log("Updating database with", data.id)
-    console.log(type)
-    res.status(200).json({ name: 'Got the webhook' });
-
-
-    const user_id = data.id
-    const user_email = data.email_addresses[0].email_address
-
-
-    // Handle the webhook
-    if (type === 'session.created') {
+    try {
+      // Wrap the API route handler with the jsonParser middleware
+      await jsonParser(req, res);
+  
+      console.log(req.body);
+      const { data, type } = req.body;
+      console.log("Updating database with", data.id);
+      res.status(200).json({ name: 'Got the webhook' });
+  
+      const user_id = data.id;
+  
+      // Handle the webhook
+      if (type === 'session.created') {
         await supabase.from('user_data').upsert({ id: user_id });
+      }
+    } catch (error) {
+      console.error("Error handling webhook:", error);
+      return res.status(500).json({ error: "Failed to handle webhook" });
     }
-    return null;
-}
+  }
 
 
