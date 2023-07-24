74a0e82296e189e76e132fe61c6bd670601ed190
Christian Armstrong <contact@thechristianarmstrong.design>
2023-07-24 17:23:22 -0500

    Trying to get it to upload to supabase now

--- pages/api/webhooks/user_sync.js
+++ pages/api/webhooks/user_sync.js
@@ -15,14 +15,13 @@ export default async function handler(req, res) {
     try {
       console.log(req.body);
       const { data, type } = req.body;
-      console.log("Updating database with", data.id);
       res.status(200).json({ name: 'Got the webhook' });
   
-      const user_id = data.id;
+      const user_id = data.user_id;
   
       // Handle the webhook
       if (type === 'session.created') {
-        await supabase.from('user_data').upsert({ id: user_id });
+        supabase.from('user_data').upsert({ id: user_id });
       }
     } catch (error) {
       console.error("Error handling webhook:", error);
