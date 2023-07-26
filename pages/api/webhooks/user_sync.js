752ee96b6056bfadfa07747c59e950f5b84b0ee6
Christian Armstrong <contact@thechristianarmstrong.design>
2023-07-25 22:22:38 -0500

    A

--- pages/api/webhooks/user_sync.js
+++ pages/api/webhooks/user_sync.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
 
 export default async function handler(req, res) {
