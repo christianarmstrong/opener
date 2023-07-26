07e7b166d8a270e19cceea9332688454aba1801d
Christian Armstrong <contact@thechristianarmstrong.design>
2023-07-26 10:12:06 -0500

    User_sync final touches

--- pages/api/webhooks/user_sync.js
+++ pages/api/webhooks/user_sync.js
@@ -7,31 +7,24 @@ const supabase = createClient(
 
 export default async function handler(req, res) {
   try {
-    const { data, type } = req.body;
-    const user_id = data.user_id;
-
-    const { data: deleteUser, error: deleteError } = await supabase
-        .from("user_data")
-        .delete()
-        .eq('id', '[object Undefined]' );
 
     // Handle the webhook
     if (type === "session.created") {
 
-      const { data: upsertUser, error: upsertError } = await supabase
-        .from("user_data")
-        .upsert([{ id: user_id, openers_created: 0, subscription: "basic" }], {
-          onConflict: ["id"],
-        });
+        const { data, type } = req.body;
+        const user_id = data.user_id;
+        const { data: upsertUser, error: upsertError } = await 
+            supabase.from("user_data").upsert([{ id: user_id, openers_created: 0, subscription: "basic" }], {onConflict: ["id"],});
 
-      if (upsertError) {
-        console.error("Upsert failed:", upsertError.message);
-        return res.status(500).json({ error: "Failed to handle webhook" });
-      }
+        if (upsertError) {
+            console.error("Upsert failed:", upsertError.message);
+            return res.status(500).json({ error: "Failed to handle webhook" });
+        }
 
-      console.log("Upsert response:", upsertUser);
-      return res.status(200).json({ name: "Upserted:" + user_id });
+        console.log("Upsert response:", upsertUser);
+        return res.status(200).json({ name: "Upserted:" + user_id });
     }
+
   } catch (error) {
     console.error("Error handling webhook:", error);
     return res.status(500).json({ error: "Failed to handle webhook" });
